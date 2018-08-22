c34bbe0434819f22834e53a2dae7082aabfad073
JustinChowBeliefChain <zhouzhiyu@beliefchain.com>
2018-08-22 21:29:00 +0800

    modify log handler

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
+const Path               = require('path');
+const Web3               = require('web3');
+const Utils              = require('util');
+const Logger             = require('bearcatjs-logger');
+Logger.configure(Path.join(__dirname, './config/logger.json'));
 
-require("./lib/Utils.js");
+const config             = require('config');
 
-let a ="letb=a.hashCode();String.prototype.hashCodeletb=a.hashCode();letb=a.hashCode();letb=a.hashCode();=function(){String.prototype.hashCode=function(){String.prototype.hashCode=function(){";
-let b = a.hashCode();
-console.log(b);
+const Service            = require('./src/services');
+const logger             = Logger.getLogger('main');
+
+logger.info('Using Config [ %j ] [ %j ]', config, process.env);
+
+let wssProvider = new Web3.providers.WebsocketProvider(config.get('rpc.providerUrl'), config.get('rpc.options'));
+
+wssProvider.on('connect', (...argv) => logger.info('Web3 Connecting.... %j', Utils.inspect(argv)));
+wssProvider.on('error', err => {
+  if (process.env.NODE_ENV === 'sphinxchain')
+    Service.sendErrorNotify(`Watch Dog Infura Connection Error ${err.message}`, err.stack);
+  logger.error('%j Web3 Wss Connection Error', new Date(), err);
+  process.exit(125);
+});
+wssProvider.on('end', (...argv) => {
+  if (process.env.NODE_ENV === 'sphinxchain')
+    Service.sendErrorNotify(`Watch Dog Infura Connection Ended`, '');
+  logger.error('%j Web3 Wss Connection End', new Date(), Utils.inspect(argv));
+  process.exit(126);
+});
+
+let web3 = new Web3(wssProvider);
+
+config.contractMetas = Path.join('node_modules/sphinx-meta', `${config.get('rpc.contractEnvName')}`, 'build/contracts')
+
+
+const {loadAllABIFromBuild} = require('./lib/Utils');
+const {contracts, address2name, topic2name} = loadAllABIFromBuild(this.config.contractMetas);
+
+logger.info('a2n      len [ %s ] [ %j ] \n', Object.keys(address2name).length , address2name          );
+logger.info('t2n      len [ %s ] [ %j ] \n', Object.keys(topic2name).length   , topic2name            );
+logger.info('contract len [ %s ] [ %j ] \n', Object.keys(contracts).length    , Object.keys(contracts));
 
---
+++ tests/test_blockHandler.js
@@ -0,0 +1,5 @@
+const Async        = require('async');
+const Logger       = require('bearcatjs-logger');
+const BlockHandler = require("../lib/BlockHandler");
+
+
